4d5da71868c50feb852c1367c1f24a75eb617704
ShubhMac <shubhamji4646@gmail.com>
2026-02-05 01:50:41 +0530

    middleware n post API

--- testRestApi.js
+++ testRestApi.js
@@ -2,6 +2,26 @@ const express  = require('express');
 const app = express();
 const port = 8000;
 const users = require('./MOCK_DATA.json')
+const fs = require('fs');
+
+// Middleware to parse JSON bodies // Plugins
+app.use(express.urlencoded({ extended: false })); // for form submissions (www-form-urlencoded)
+app.use(express.json()); // for json bodies
+
+app.use((req, res, next) => {
+  // return res.json({
+  //   Status: "Stuck in II Middleware",
+  // });  // if we do not use next(), it will stuck here
+  fs.appendFile('./middleware.txt', `A user requested to server having ip ${req.hostname} at ${new Date().toISOString()}\n`, (err,data)=> {});
+  next();
+}); // Custom Middleware example
+  
+app.use((req, res, next) => {
+  // return res.json({
+  //   Status: "Stuck in III Middleware",
+  // });  // if we do not use next(), it will stuck here
+  next();
+}); // Custom Middleware example
 
 app.get('/users', (req, res) => {
   res.send(users);
@@ -23,6 +43,58 @@ app.get('/api/users/:id', (req, res) => {
     }
 });
 
+app.post('/api/users', (req, res) => {
+  let auto_incrementId = users.length + 1;
+  const body = req.body;
+  // console.log(body);
+  users.push({
+    id: auto_incrementId,
+    first_name: body.first_name,
+    last_name: body.last_name,
+    email: body.email,
+    gender:body.gender,
+    job_title: body.job_title});
+    // or users.push({...body, id: auto_incrementId});
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+    return res.json(
+        {
+          status: "Success",
+          message: "User created successfully",
+          id:auto_incrementId
+        });
+    });
+  
+});
+
+app.patch('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const body = req.body;
+  const userIndex = users.findIndex(u => u.id === userId);
+  if (userIndex !== -1) {
+    users[userIndex] = { ...users[userIndex], ...body }; // it means only update the fields present in body
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+      return res.json({
+        status: "Success",
+        message: "User updated successfully"
+      });
+    });
+  } else {
+    res.status(404).json({ 
+      status: "Error", 
+      status_code: 404,
+      message: "User not found" 
+    });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Test REST API server is running on http://localhost:${port}`);
+  setTimeout(() => {
+  console.log('Server is now ready!');
+  }, 2000); 
+  setTimeout(() => {
+    console.log('You can test the API endpoints now!');
+  }, 3000); 
 });
